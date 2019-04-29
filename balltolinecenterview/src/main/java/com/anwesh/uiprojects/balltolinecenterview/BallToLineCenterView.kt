1ea3bc575b34cec7ea6486f7a31d8a8b4dc301a1
Anwesh43 <anweshthecool0@gmail.com>
2019-04-30 00:58:19 +0530

    adding libraries and constants

--- balltolinecenterview/src/main/java/com/anwesh/uiprojects/balltolinecenterview/BallToLineCenterView.kt
+++ balltolinecenterview/src/main/java/com/anwesh/uiprojects/balltolinecenterview/BallToLineCenterView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.balltolinecenterview
 /**
  * Created by anweshmishra on 30/04/19.
  */
+
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+
+
+val nodes : Int = 5
+val parts : Int = 2
+val subParts : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
