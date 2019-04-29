1a6c6dc504478d4cb47e1b69e8da6d92c5886a2f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-30 01:25:56 +0530

    adding BTLCNode class

--- balltolinecenterview/src/main/java/com/anwesh/uiprojects/balltolinecenterview/BallToLineCenterView.kt
+++ balltolinecenterview/src/main/java/com/anwesh/uiprojects/balltolinecenterview/BallToLineCenterView.kt
@@ -126,4 +126,48 @@ class BallToLineCenterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTLCNode(var i : Int, val state : State = State()) {
+
+        private var next : BTLCNode? = null
+        private var prev : BTLCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BTLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTLCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTLCNode {
+            var curr : BTLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
